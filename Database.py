9b7066d8ca1634f0880b93d0b882b19801022a0d
Hymed <hymedcool@live.fr>
2020-12-08 04:03:26 +0100

    Interface graphique, intro + menu + formulaire de produit et de catégorie

--- Database.py
+++ Database.py
@@ -25,15 +25,10 @@ class Database:
     """
 
     def __init__(self):
-        self.__host = "localhost"
-        self.__user = "root"
-        self.__passwd = ""
-        self.__db = "amazon"
-        self.__connector = ""
-        self.__cursor = ""
-        self.__header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
-                                       'like Gecko) '
-                                       'Chrome/58.0.3029.110 Safari/537.36'}
+
+        self.__header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/58.0.3029.110 Safari/537.36'}
         self.__title = "Amazon Scrapp"
         self.__welcome = "Bienvenue dans l'Amazon Scrapp !"
         self.__help = "Pour la liste des commandes tapez : ?"
@@ -46,16 +41,19 @@ class Database:
             "supprimer produit": "Permet de supprimer un produit"
         }
 
-    def start(self):
+    @staticmethod
+    def start():
         try:
-            self.__connector = mysql.connector.connect(host=self.__host, user=self.__user, passwd=self.__passwd,
-                                                       db=self.__db)
+            connector = mysql.connector.connect(host="localhost", user="root", passwd="", db="amazon")
+            return connector
         except mysql.connector.Error as error:
             print("Impossible de se connecter à la base de données: {}".format(error))
 
-    def select_product(self):
+    @staticmethod
+    def select_product():
         try:
-            cursor = self.__connector.cursor()
+            conn = BddConnection.start()
+            cursor = conn.cursor()
             product = cursor.execute("""SELECT * from produits """)
             rows = cursor.fetchall()
             for row in rows:
@@ -66,104 +64,136 @@ class Database:
         except mysql.connector.Error as error:
             print("Pas de produit : {}".format(error))
 
-    def select_categories(self):
+    @staticmethod
+    def get_cat_name():
         try:
-            cursor = self.__connector.cursor()
-            product = cursor.execute("""SELECT * from catégories """)
+            conn = BddConnection.start()
+            cursor = conn.cursor()
+            cat = cursor.execute("""SELECT Cat_nom from catégories """)
             rows = cursor.fetchall()
-            print(type(rows))
+            cat_list = []
             for row in rows:
-                print("Id = ", row[0], )
-                print("Catégorie  = ", row[1], "\n")
+                cat_list.append(row[0])
+            return cat_list
         except mysql.connector.Error as error:
             print("Pas de catégorie : {}".format(error))
 
-    def new_product(self, nom, url, cat, prix):
-        cursor = self.__connector.cursor()
+    @staticmethod
+    def select_categories():
+        try:
+            conn = BddConnection.start()
+            cursor = conn.cursor()
+            cat = cursor.execute("""SELECT * from catégories """)
+            row1 = cursor.fetchall()
+            for row in row1:
+                print("Id = ", row[0], )
+                print("Catégorie  = ", row[1], "\n")
+
+        except mysql.connector.Error as error:
+            print("Pas de catégorie : {}".format(error))
+
+    @staticmethod
+    def new_product(nom, url, cat, prix):
+        conn = BddConnection.start()
+        cursor = conn.cursor()
         insert_tuple = (nom, url, cat, prix)
         print(insert_tuple)
         insert_query = """INSERT INTO produits (Nom, Url, Catégorie, prix) 
                             VALUES (%s, %s, %s, %s)"""
         try:
             insert = cursor.execute(insert_query, insert_tuple)
-            self.__connector.commit()
+            conn.commit()
             print('Produit bien enregisté !')
         except mysql.connector.Error as error:
             print("le produit n'a pas été enregistré : {}".format(error))
 
-    def update_product(self, id, nom, url, cat):
-        cursor = self.__connector.cursor()
+    @staticmethod
+    def update_product(id, nom, url, cat):
+        conn = BddConnection.start()
+        cursor = conn.cursor()
         insert_tuple = (nom, url, cat, id)
         insert_query = """UPDATE produits SET Nom = %s, Url = %s, Catégorie = %s WHERE Id = %s"""
         try:
             insert = cursor.execute(insert_query, insert_tuple)
-            self.__connector.commit()
+            conn.commit()
             print('Produit mis à jour !')
         except mysql.connector.Error as error:
             print("le produit n'a pas été mis à jour : {}".format(error))
 
-    def new_categories(self, nom):
-        cursor = self.__connector.cursor()
+    @staticmethod
+    def new_categories(nom):
+        conn = BddConnection.start()
+        cursor = conn.cursor()
         insert_tuple = (nom)
         insert_query = """INSERT INTO catégories (Cat_Nom) 
                             VALUES (%s)"""
         try:
             insert = cursor.execute(insert_query, (insert_tuple,))
-            self.__connector.commit()
+            conn.commit()
             print('Catégorie bien enregisté !')
         except mysql.connector.Error as error:
             print("La catégorie n'a pas été enregistré : {}".format(error))
 
-    def delete_categories(self, id):
-        cursor = self.__connector.cursor()
+    @staticmethod
+    def delete_categories(id):
+        conn = BddConnection.start()
+        cursor = conn.cursor()
         delete_tuple = (id)
         delete_query = "DELETE FROM catégories WHERE Cat_Id = %s"
         try:
             delete = cursor.execute(delete_query, (delete_tuple,))
-            self.__connector.commit()
+            conn.commit()
             print('Catégorie bien supprimé !')
         except mysql.connector.Error as error:
             print("La catégorie n'a pas été supprimé : {}".format(error))
 
-    def delete_product(self, id):
-        cursor = self.__connector.cursor()
+    @staticmethod
+    def delete_product(id):
+        conn = BddConnection.start()
+        cursor = conn.cursor()
         delete_tuple = (id)
         delete_query = "DELETE FROM produits WHERE Id = %s"
         try:
             delete = cursor.execute(delete_query, (delete_tuple,))
-            self.__connector.commit()
+            conn.commit()
             print('Produit bien supprimé !')
         except mysql.connector.Error as error:
             print("Le produit n'a pas été supprimé : {}".format(error))
 
-    def intro(self):
+    @staticmethod
+    def intro():
         drawing = Figlet(font='slant')
-        print(drawing.renderText(self.__title))
+        print(drawing.renderText("Amazon Scrapp"))
 
-    def welcome(self):
-        print(self.__welcome)
-        print(self.__help)
+    @staticmethod
+    def welcome():
+        print("Bienvenue dans l'Amazon Scrapp !")
+        print("Pour la liste des commandes tapez : ?")
 
     def list_command(self):
         for key, value in self.__command.items():
             print(key, '=>', value)
 
-    def get_by_url(self, new_url):
+    @staticmethod
+    def get_by_url(new_url):
         url_valid = validators.url(new_url)
-        if url_valid == True:
+        if url_valid:
             try:
-                page = requests.get(new_url, headers=self.__header)
+                page = requests.get(new_url, headers={
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                  'Chrome/58.0.3029.110 Safari/537.36'})
                 soup = BeautifulSoup(page.content, "html.parser")
-                new_price = soup.find(id="priceblock_ourprice").get_text()
-                print(new_price)
-                print(type(new_price))
-                while new_price == None:
-                    self.get_by_url(new_url)
+                price_euro = soup.find(id="priceblock_ourprice").get_text()
+                while price_euro is None:
+                    BddConnection.get_by_url(new_url)
+                trim = re.compile(r'[^\d.,]+')
+                price_coma = trim.sub('', price_euro)
+                new_price = float(price_coma.replace(',', '.'))
                 return new_price
             except:
-                print("L'url n'est pas valide.")
+                print("L'url n'est pas valide")
         else:
-            print("L'url n'est pas valide")
+            print("Ce n'est pas un URl valide")
 
     def command(self):
         user_input = input()
@@ -174,11 +204,11 @@ class Database:
         elif user_input == 'catégories':
             self.select_categories()
         elif user_input == 'nouveau produit':
-            # new_name = input("Entrez le nom du produit\n")
-            # new_cat = input("Entrez la catégorie du produit\n")
+            new_name = input("Entrez le nom du produit\n")
+            new_cat = input("Entrez la catégorie du produit\n")
             new_url = input("Entrez l'url amazon du produit\n")
             new_price = self.get_by_url(new_url)
-            # self.new_product(new_name,new_url,new_cat,int(new_price))
+            self.new_product(new_name, new_url, new_cat, new_price)
         elif user_input == 'nouvelle catégorie':
             new_name = input('Entrez le nom de la nouvelle catégorie\n')
             self.new_categories(new_name)
--- Gui.py
+++ Gui.py
@@ -1,18 +1,22 @@
 # coding: utf-8
 import kivy
 from kivy.app import App
+from BddConnection import BddConnection
 from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 
+Window.clearcolor = (0.1, 0.5, 0.6, 1)
 Config.set('graphics', 'resizable', 0)
 Config.set('graphics', 'width', '1000')
 Config.set('graphics', 'height', '700')
 Config.write()
+
 
 class MainWindow(Screen):
     pass
@@ -21,13 +25,34 @@ class MainWindow(Screen):
 class MenuWindow(Screen):
     pass
 
+
 class NewProductWindow(Screen):
-    pass
+    def spinner(self):
+        return BddConnection.get_cat_name()
+
+    def new_product(self, nom, url, cat):
+        if nom == "":
+            print("Le nom est vide")
+        elif url == "":
+            print("L'url est vide")
+        else:
+            try:
+                price = BddConnection.get_by_url(url)
+                print(price)
+                BddConnection.new_product(nom, url, cat, price)
+            except:
+                print("L'enregistrement n'a pas été effectuée")
+
+class NewCatWindow(Screen):
+    def return_cat(self, categorie):
+        if categorie:
+            nom_cat = str(categorie)
+            print(nom_cat)
+            BddConnection.new_categories(nom_cat)
 
 
 class WindowManager(ScreenManager):
     pass
-
 
 
 kv = Builder.load_file("intro.kv")
@@ -37,6 +62,3 @@ class AmazonScrappApp(App):
 
     def build(self):
         return kv
-
-
-AmazonScrappApp().run()
